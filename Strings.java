7eda5420207eeb8f854475e39463e69d40520fd4
Aaryan Vijayvargiya <aaryanvijay12345@gmail.com>
2024-04-12 17:05:44 +0530

    Palindrome question

--- Strings.java
+++ Strings.java
@@ -1,22 +1,30 @@
 import java.util.*;
 
 public class Strings {
-    public static void main(String args[]) {
-        char arr[] = { 'a', 'b', 'c', 'd' };
-        String str = "abcd";
-        String str1 = new String("xyz");
 
-        Scanner sc = new Scanner(System.in);
-        String name;
-        name = sc.nextLine();
-        System.out.println(name);
-        System.err.println(name.length());
+    public static void printLetters(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            System.out.print(str.charAt(i) + " ");
+        }
+    }
+
+    public static void main(String args[]) {
+        // char arr[] = { 'a', 'b', 'c', 'd' };
+        // String str = "abcd";
+        // String str1 = new String("xyz");
+
+        // Scanner sc = new Scanner(System.in);
+        // String name;
+        // name = sc.nextLine();
+        // System.out.println(name);
+        // System.err.println(name.length());
 
         // Concatanation
         String firstName = "Aaryan";
         String secondName = "Vijay";
         String fullName = firstName + " " + secondName;
-        System.out.println(fullName);
+        // System.out.println(fullName.charAt(0));
+        printLetters(fullName);
 
     }
 }
